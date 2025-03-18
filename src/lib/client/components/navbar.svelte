8aed553e9494b9728ced100f14c045e3aac9013e
dastin <astindillon@gmail.com>
2025-03-17 21:57:34 -0400

    init commit

---
+++ src/lib/client/components/navbar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<div class="navbar bg-base-200 rounded-2xl">
+  <div class="navbar-start md:hidden">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li>
+          <a href="/home" aria-current={page.url.pathname === '/home'}>Home</a>
+        </li>
+        <li>
+          <a href="/login" aria-current={page.url.pathname === '/login'}>Login</a>
+        </li>
+        <li>
+          <a href="/demo" aria-current={page.url.pathname === '/demo'}>Demo</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-start hidden md:flex">
+    <a class="btn btn-ghost text-xl" href="/">My Wishlist</a>
+  </div>
+  <div class="navbar-center md:hidden">
+    <a class="btn btn-ghost text-xl" href="/">My Wishlist</a>
+  </div>
+  <div class="hidden navbar-center md:flex">
+    <ul class="menu menu-horizontal px-1">
+      <li>
+        <a href="/home" aria-current={page.url.pathname === '/home'}>Home</a>
+      </li>
+      <li>
+        <a href="/login" aria-current={page.url.pathname === '/login'}>Login</a>
+      </li>
+      <li>
+        <a href="/demo" aria-current={page.url.pathname === '/demo'}>Demo</a>
+      </li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <button class="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
+    </button>
+    <button class="btn btn-ghost btn-circle">
+      <div class="indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
+        <span class="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+  </div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/client/components/navbar.svelte';
+	import Dock from '$lib/client/components/dock.svelte';
+
+	let { children } = $props();
+</script>
+
+
+<div class="flex justify-center mt-8">
+	<div class="flex flex-col w-full max-w-4xl">
+
+		<Navbar></Navbar>
+	
+		<div class="bg-base-200 mt-8 min-h-dvh rounded-2xl p-4">
+			{@render children()}
+		</div>
+
+		<Dock></Dock>
+	</div>
+</div>
+
+
