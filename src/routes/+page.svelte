cbce318a2670b376917b61c4cd848e4273a7deed
dastin <astindillon@gmail.com>
2025-03-17 22:37:18 -0400

    updated landing page. updated daisyui version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="hero bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">My Wishlist</h1>
+			<p class="py-6">
+				An easy way to share your wishlists with others.
+			</p>
+			<a class="btn btn-primary" href="/login">Login</a>
+		</div>
+	</div>
+</div>
