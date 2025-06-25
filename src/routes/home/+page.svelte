6032ae705e92aeb6b078b0c3596aa1630b43fd71
dastin <astindillon@gmail.com>
2025-06-25 16:33:16 -0400

    update home page with logout

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,3 +1,17 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+  let {data} = $props();
+</script>
+
+
 <div>
-  
+  <h1>Welcome, {data.user.username}</h1>
+
+
+  <div class="flex justify-end">
+    <form action="?/logout" method="post" use:enhance>
+      <button class="btn btn-secondary">Logout</button>
+    </form>
+  </div>
 </div>
